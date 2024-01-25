c7378f80f01c03d101024de2a2c334c82875c532
Rohan Verma <rv725555@gmail.com>
2024-01-25 11:57:35 +0530

    feat: Made test outcome consistent.

--- src/test/suite/clipboard.test.ts
+++ src/test/suite/clipboard.test.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import { EXTENSION_NAME } from '../../constants';
-import { writeNNumbersToClipboardOneByOne } from '../testUtils';
+import { getCompletionItemsList, writeNNumbersToClipboardOneByOne } from '../testUtils';
 
 suite('Clipboard Functionality Test Suite', () => {
     test('Check if `numberOfClipboardItems` limit being enforced', async function() {
@@ -21,31 +21,26 @@ suite('Clipboard Functionality Test Suite', () => {
 
         await writeNNumbersToClipboardOneByOne(numberOfClipboardItems + 1);
 
-        // Create a mock document
-       const uri = vscode.Uri.parse('untitled:./test.txt', true);
-       const mockDocument = await vscode.workspace.openTextDocument(uri);
-       const editor = await vscode.window.showTextDocument(mockDocument);
+       const completionList: vscode.CompletionList = await getCompletionItemsList();
 
-       // Trigger the completion provider.
-       const position = new vscode.Position(0, 0);
-       const completionList = await vscode.commands.executeCommand<vscode.CompletionList>(
-           'vscode.executeCompletionItemProvider',
-           uri,
-           position,
-           '%'
-       );
+    //    console.log(JSON.stringify(completionList));
 
        const completionItems = completionList.items.map((item) => item.insertText);
 
+    //    console.log(JSON.stringify(completionItems));
+
        // The number of items won't be less, because we are copying N + 1 items to clipboard.
-       assert.equal(
+       assert.strictEqual(
         completionItems.length, 
         numberOfClipboardItems, 
         `${completionItems.length} stored in clipboard. Max allowed is ${numberOfClipboardItems}.`
        );
 
        for(let i = 1; i <= numberOfClipboardItems; i++) {
-          assert.equal(completionItems[i - 1], i, `${completionItems[i - 1]} present at position ${i}. Expected ${i}.`);
+          assert.strictEqual(
+            completionItems[i - 1], 
+            i.toString(), 
+            `${completionItems[i - 1]} present at position ${i}. Expected ${i}.`);
        }
 
     //    console.log(`completionList: ${JSON.stringify(completionList)}`);
--- src/test/testUtils.ts
+++ src/test/testUtils.ts
@@ -8,15 +8,72 @@ export async function writeNNumbersToClipboardOneByOne(
 
     const { clipboardPollInterval } = workspaceConfig; 
 
-    return new Promise((resolve, reject) => {
-        const interval = setInterval(() => {
-            vscode.env.clipboard.writeText(n.toString());
+    // return new Promise((resolve, reject) => {
+    //     let tryingToWrite = n + 1;
+    //     let previouslyWritten = n + 1;
+
+    //     const interval = setInterval(async () => {
+    //         // console.log(`tried N: ${n}`);
+
+    //         // The number we tried to write should have already been written.
+    //         if(previouslyWritten !== tryingToWrite) {
+    //             return;
+    //         }
+
+    //         // The current number should be exactly 1 less than the number already written.
+    //         if(previouslyWritten - n !== 1) {
+    //             return;
+    //         }
+    //         tryingToWrite = n;
+
+    //         // console.log(`tryingToWrite: ${tryingToWrite}`);
+
+    //         await vscode.env.clipboard.writeText(n.toString());
+    //         // console.log(`${n}: ${await vscode.env.clipboard.readText()}`);
+    //         previouslyWritten = n;
+    //         n--;
+
+    //         if(n === 0) {
+    //             clearInterval(interval);
+    //             /* 
+    //             This timeout is not necessary for ensuring that text is written to clipboard, it is for ensuring 
+    //             that the clipboard is polled for the last number which is 1, and that item is added to the completion
+    //             item array of the extension
+    //             */
+    //             await new Promise(resolve => {setTimeout(() => resolve(true), 2 * clipboardPollInterval);});
+    //             resolve(0);
+    //         }
+    //     }, 2 * clipboardPollInterval); 
+    //     // We choose double the clipboardPollInterval to allow for inconsistencies in clipboard polling
+    // });
+
+    return new Promise(async (resolve, reject) => {
+        while(n > 0) {
+            await vscode.env.clipboard.writeText(n.toString());
             n--;
 
-            if(n === 0) {
-                clearInterval(interval);
-                resolve(0);
-            }
-        }, 2 * clipboardPollInterval);
+            // Waiting for the item to be added to CompletionItem array.
+            await new Promise((resolve) => setTimeout(resolve, clipboardPollInterval * 2, true));
+        }
+
+        resolve(true);
     });
+}
+
+
+export async function getCompletionItemsList() {
+    // Create a mock document
+    const uri = vscode.Uri.parse('untitled:./test.txt', true);
+    const mockDocument = await vscode.workspace.openTextDocument(uri);
+    const editor = await vscode.window.showTextDocument(mockDocument);
+
+    // Trigger the completion provider.
+    const position = new vscode.Position(0, 0);
+
+    return vscode.commands.executeCommand<vscode.CompletionList>(
+        'vscode.executeCompletionItemProvider',
+        uri,
+        position,
+        '%'
+    );
 }
